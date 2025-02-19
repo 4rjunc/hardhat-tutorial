5177cb3a3eabbd0adbeca3426056c78b5719c1b9
4rjunc <arjunmundamani@gmail.com>
2025-02-20 00:06:00 +0530

    Deployed!

---
+++ README.md
@@ -0,0 +1,1 @@
+DEPLOYED : [HERE](https://sepolia.arbiscan.io/address/0x0bdBBD70a339B84FC19265081b947413267f0578)
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://1rpc.io/sepolia`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
